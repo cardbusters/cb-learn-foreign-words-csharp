61fdfbe5a76b349b9f351c769702106e1b1bd3f3
slange <slange@igus.net>
2022-04-22 13:39:22 +0200

    tests: added teacher asks for to sleep

--- LearnForeignWords.Tests/UnitTest1.cs
+++ LearnForeignWords.Tests/UnitTest1.cs
@@ -4,9 +4,18 @@ namespace LearnForeignWords.Tests;
 
 public class UnitTest1
 {
+
     [Fact]
-    public void Test1()
+    public void WhenTeachAsksThenToSleepShouldBeReturned()
     {
-        
+        Assert.Equal("to sleep", Teacher.ask());
+    }
+}
+
+internal class Teacher
+{
+    internal static string ask()
+    {
+        return "to sleep";
     }
 }
