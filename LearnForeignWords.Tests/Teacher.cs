d0478024e85dde01a118e32879036de31e7b82d2
Tugbay Atilla <Tugbay Atilla tatilla@igus.net>
2022-04-22 14:10:30 +0200

    feat: teacher asks question as string

---
+++ LearnForeignWords.Tests/Teacher.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LearnForeignWords.Tests
+{
+    public class Teacher
+    {
+        internal static string ask()
+        {
+            return "";
+        }
+    }
+}
---
+++ LearnForeignWords.Tests/TeacherTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace LearnForeignWords.Tests;
+
+public class TeacherTests
+{
+    [Fact]
+    public void TeacherAsksQuestionThenResultShouldBeString()
+    {
+        Assert.IsType<string>(Teacher.ask());
+    }
+}
